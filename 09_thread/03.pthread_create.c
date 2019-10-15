4f8e2cc6125fac9cc5237060377dab2e1e6eb581
wjf <944926942@qq.com>
2019-10-15 20:19:42 +0800

    创建子线程

--- 09_thread/03.pthread_create.c
+++ 09_thread/03.pthread_create.c
@@ -3,13 +3,27 @@
 #include <unistd.h>
 #include <pthread.h>
 
+void * threadaction(void * arg)
+{
+    printf("thread: pid=%d, tid=%lu\n",getpid(), pthread_self());
+    return NULL;
+}
+
 int main()
 {
     pthread_t tid;
     //获取线程ID
-    tid = pthread_self();    
-    printf("main: pid=%d, tid=%lu\n",getpid(), tid);
-     
+    printf("main: pid=%d, tid=%lu\n",getpid(), pthread_self());
 
+    //创建线程
+    int ret = pthread_create(&tid, NULL, threadaction, NULL);
+    if(0 != ret)
+    {
+        perror("pthread_create error");
+        exit(1);
+    }
+    
+    //如果没有sleep函数，主线程结束，系统回收进程地址空间，而子线程是与主线程共享地址空间的
+    sleep(1);
     return 0;
 }
