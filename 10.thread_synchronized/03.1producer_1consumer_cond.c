53848dab1f0575b387d342284f5bcc2791b1caea
wjf <944926942@qq.com>
2019-10-21 13:03:12 +0800

    条件变量实现单生产者单消费者n缓冲区模型

--- 10.thread_synchronized/03.1producer_1consumer_cond.c
+++ 10.thread_synchronized/03.1producer_1consumer_cond.c
@@ -33,40 +33,46 @@ void err_pthread(int ret, const char * str)
 
 void * consumter(void * arg)
 {
-    pthread_mutex_lock(&mymutex);   //加锁互斥量
-    if(NULL == head)
+    while(1)
     {
-        pthread_cond_wait(&mycond, &mymutex);     //阻塞等待条件变量
-        //若条件不满足,需要阻塞等待
-        //若条件不满足,则阻塞等待并解锁;临界区没有产品，应该解锁并且让生产者获得临界区;
-        //若条件满足(被生成者线程调用pthread_cond_signal函数通知),解除阻塞并加锁 
+        pthread_mutex_lock(&mymutex);   //加锁互斥量
+        if(NULL == head)
+        {
+            pthread_cond_wait(&mycond, &mymutex);     //阻塞等待条件变量
+            //若条件不满足,需要阻塞等待
+            //若条件不满足,则阻塞等待并解锁;临界区没有产品，应该解锁并且让生产者获得临界区;
+            //若条件满足(被生成者线程调用pthread_cond_signal函数通知),解除阻塞并加锁 
+        }
+
+        //从头指针指向的节点开始删除
+        printf("---consumer:[%d]\n", head->data);
+        Node * temp = head;
+        head = head->next;
+        free(temp);
+        temp = NULL;
+
+        pthread_mutex_unlock(&mymutex);
+        sleep(rand() % 3); 
     }
-
-    //从头指针指向的节点开始删除
-    printf("consumer:[%d]\n", head->data);
-    Node * temp = head;
-    head = head->next;
-    free(temp);
-    temp = NULL;
-
-    pthread_mutex_lock(&mymutex);
     return NULL;
 }
 
 void * producer(void * arg)
 {
-    Node * tempnode = (Node *)malloc(sizeof(Node));
-    tempnode->data = rand() % 1000 + 1;     //1~1000
-    printf("producer:[%d]\n", tempnode->data);
-    pthread_mutex_lock(&mymutex);   //加锁互斥量
-    //头插法建立链表，head不断移动，指向新创建的节点
-    tempnode->next = head;
-    head = tempnode;
-    pthread_mutex_unlock(&mymutex);      //解锁互斥量
-    pthread_cond_signal(&mycond);     //通知pthread_cond_wait()
+    while(1)
+    {
+        Node * tempnode = (Node *)malloc(sizeof(Node));
+        tempnode->data = rand() % 1000 + 1;     //1~1000
+        printf("producer:[%d]\n", tempnode->data);
+        pthread_mutex_lock(&mymutex);   //加锁互斥量
+        //头插法建立链表，head不断移动，指向新创建的节点
+        tempnode->next = head;
+        head = tempnode;
+        pthread_mutex_unlock(&mymutex);      //解锁互斥量
+        pthread_cond_signal(&mycond);     //通知pthread_cond_wait()
 
-    sleep(rand() % 3);     //睡眠几秒，让出cpu时间片给消费者线程
-
+        sleep(rand() % 3);     //睡眠几秒，让出cpu时间片给消费者线程
+    }
     return NULL;
 }
 
@@ -82,7 +88,7 @@ int main()
     err_pthread(ret, "pthread_create");
 
 
-    pthread_join(&producer_thread, NULL);     //回收生产者线程
-    pthread_join(&consumer_thread, NULL);     //回收消费者线程
+    pthread_join(producer_thread, NULL);     //回收生产者线程
+    pthread_join(consumer_thread, NULL);     //回收消费者线程
     return 0;
 }
